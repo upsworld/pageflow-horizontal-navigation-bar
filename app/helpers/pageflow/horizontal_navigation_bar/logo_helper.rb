72b241f569a491be74c07c078668315c9bffb4ba
Christoph Merkelbach <cmerkelbach@codevise.de>
2015-06-02 17:33:17 +0200

    adds helper for injecting logo link into bar

---
+++ app/helpers/pageflow/horizontal_navigation_bar/logo_helper.rb
@@ -0,0 +1,15 @@
+module Pageflow
+  module HorizontalNavigationBar
+    module LogoHelper
+      def horizontal_navigation_bar_logo_link(entry)
+        if HorizontalNavigationBar.logo_link_url.present?
+          link_to(HorizontalNavigationBar.logo_link_text,
+                  HorizontalNavigationBar.logo_link_url,
+                  class: 'logo')
+        else
+          content_tag(:div, '', class: 'logo')
+        end
+      end
+    end
+  end
+end
--- lib/pageflow-horizontal-navigation-bar.rb
+++ lib/pageflow-horizontal-navigation-bar.rb
@@ -2,6 +2,8 @@ require 'pageflow/horizontal_navigation_bar/engine'
 
 module Pageflow
   module HorizontalNavigationBar
+    mattr_accessor :logo_link_url
+    mattr_accessor :logo_link_text
     def self.plugin
       HorizontalNavigationBar::Plugin.new
     end
