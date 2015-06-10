d3385436ebc38624a952395ae395ebdedc4817a1
Tim Fischbach <tfischbach@codevise.de>
2015-06-10 19:08:50 +0200

    Make Rendering of Thumbnails Configurable

--- lib/pageflow-horizontal-navigation-bar.rb
+++ lib/pageflow-horizontal-navigation-bar.rb
@@ -4,12 +4,20 @@ module Pageflow
   module HorizontalNavigationBar
     mattr_accessor :logo_link_url
     mattr_accessor :logo_link_text
+
+    mattr_accessor :render_thumbnails
+    self.render_thumbnails = true
+
     def self.plugin
       HorizontalNavigationBar::Plugin.new
     end
 
     def self.widget_type
       HorizontalNavigationBar::WidgetType.new
+    end
+
+    def self.configure
+      yield(self)
     end
   end
 end
