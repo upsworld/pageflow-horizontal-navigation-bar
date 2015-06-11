052cfa4bf4a3bbae4d48efea269deedc6613f6b1
Tim Fischbach <tfischbach@codevise.de>
2015-06-11 14:46:38 +0200

    Make Logo Link Target Blank

--- app/helpers/pageflow/horizontal_navigation_bar/logo_helper.rb
+++ app/helpers/pageflow/horizontal_navigation_bar/logo_helper.rb
@@ -5,6 +5,7 @@ module Pageflow
         if HorizontalNavigationBar.logo_link_url.present?
           link_to(HorizontalNavigationBar.logo_link_text,
                   HorizontalNavigationBar.logo_link_url,
+                  target: '_blank',
                   class: 'logo')
         else
           content_tag(:div, '', class: 'logo')
